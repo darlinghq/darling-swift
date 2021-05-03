a713ab13ebffe88923a2066ad8c9133cb4fe8636
Ariel Abreu <facekapow@outlook.com>
2021-05-03 19:09:39 -0400

    Fix mistake in post-checkout script

--- post-checkout.sh
+++ post-checkout.sh
@@ -3,11 +3,11 @@ set -e
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-VERSION="$(cat version.txt)"
+VERSION="$(cat "${SCRIPT_DIR}/version.txt")"
 REPO_NAME="darlinghq/darling-swift"
 TAG_NAME="v${VERSION}"
 FILE_NAME="swift-${VERSION}.zip"
 URL="https://github.com/${REPO_NAME}/releases/download/${TAG_NAME}/${FILE_NAME}"
 
 curl -s "${URL}" -o "${SCRIPT_DIR}/${FILE_NAME}" -L
-unzip "${SCRIPT_DIR}/${FILE_NAME}" -d "${SCRIPT_DIR}"
+unzip -o "${SCRIPT_DIR}/${FILE_NAME}" -d "${SCRIPT_DIR}"
